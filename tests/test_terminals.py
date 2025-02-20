7e7be77bacbcc1650070cb2ae7d8f100dc64f2a1
Nicolas <Nicolas@nonan.net>
2025-02-20 17:14:23 +0100

    Test modified to allow for in- and output digital terminals

--- tests/test_terminals.py
+++ tests/test_terminals.py
@@ -15,15 +15,16 @@ def test_terminal_plausib():
                                             DigitalOutputTerminal,
                                             AnalogInputTerminal,
                                             AnalogOutputTerminal]:
+
             print('Terminal: ' + n)
             if issubclass(o, DigitalInputTerminal):
                 assert o.parameters.get('input_bit_width', 0) > 0
-                assert o.parameters.get('output_bit_width', 0) == 0
+                # assert o.parameters.get('output_bit_width', 0) == 0
                 assert o.parameters.get('input_word_width', 0) == 0
                 assert o.parameters.get('output_word_width', 0) == 0
 
             if issubclass(o, DigitalOutputTerminal):
-                assert o.parameters.get('input_bit_width', 0) == 0
+                # assert o.parameters.get('input_bit_width', 0) == 0
                 assert o.parameters.get('output_bit_width', 0) > 0
                 assert o.parameters.get('input_word_width', 0) == 0
                 assert o.parameters.get('output_word_width', 0) == 0
